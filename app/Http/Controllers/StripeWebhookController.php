79058b60183163baa8522069d1702b36349263b8
Hamaad Kaleem <132321495+Hamaad641@users.noreply.github.com>
2024-02-09 22:39:44 +0500

    Updated StripeWebhookController.php and AwardCredit.php

--- app/Http/Controllers/StripeWebhookController.php
+++ app/Http/Controllers/StripeWebhookController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\AwardCredit;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Laravel\Cashier\Http\Controllers\WebhookController;
-use Log;
 
 class StripeWebhookController extends WebhookController
 {
@@ -16,6 +18,49 @@ class StripeWebhookController extends WebhookController
      */
     public function handleWebhook(Request $request)
     {
-        return parent::handleWebhook($request);
+        return $this->handleCustomerSubscriptionCreated($request->all());
+    }
+
+    protected function handleCustomerSubscriptionCreated(array $payload)
+    {
+        $user = $this->getUserByStripeId($payload['data']['object']['customer']);
+
+        if ($user) {
+            $data = $payload['data']['object'];
+
+            if (! $user->subscriptions->contains('stripe_id', $data['id'])) {
+                if (isset($data['trial_end'])) {
+                    $trialEndsAt = Carbon::createFromTimestamp($data['trial_end']);
+                } else {
+                    $trialEndsAt = null;
+                }
+
+                $firstItem = $data['items']['data'][0];
+                $isSinglePrice = count($data['items']['data']) === 1;
+
+                $subscription = $user->subscriptions()->create([
+                    'name' => $data['metadata']['name'] ?? $this->newSubscriptionName($payload),
+                    'stripe_id' => $data['id'],
+                    'stripe_status' => $data['status'],
+                    'stripe_price' => $isSinglePrice ? $firstItem['price']['id'] : null,
+                    'quantity' => $isSinglePrice && isset($firstItem['quantity']) ? $firstItem['quantity'] : null,
+                    'trial_ends_at' => $trialEndsAt,
+                    'ends_at' => null,
+                ]);
+
+                foreach ($data['items']['data'] as $item) {
+                    $subscription->items()->create([
+                        'stripe_id' => $item['id'],
+                        'stripe_product' => $item['price']['product'],
+                        'stripe_price' => $item['price']['id'],
+                        'quantity' => $item['quantity'] ?? null,
+                    ]);
+                }
+            }
+
+            dispatch(new AwardCredit($user->id));
+        }
+
+        return $this->successMethod();
     }
 }
