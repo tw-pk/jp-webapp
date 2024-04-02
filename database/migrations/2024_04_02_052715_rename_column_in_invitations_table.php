2b262b5caea2eedc22bdb29fb33c876c043b3ff2
Usman Ghani <usman.g@teamswork.pk>
2024-04-02 10:31:56 +0500

    invitation column name update

---
+++ database/migrations/2024_04_02_052715_rename_column_in_invitations_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('invitations', function (Blueprint $table) {
+            $table->renameColumn('cam_have_new_number', 'can_have_new_number');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('invitations', function (Blueprint $table) {
+            $table->renameColumn('can_have_new_number', 'cam_have_new_number');
+        });
+    }
+};
