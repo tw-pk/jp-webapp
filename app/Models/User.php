509063f444de59edec4752b503ff1f80dc1a5a42
Usman Ghani <usman.g@teamswork.pk>
2024-05-27 19:07:39 +0500

    defaultPaymentMethod function in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -69,9 +69,14 @@ class User extends Authenticatable implements MustVerifyEmail
         'deleted_at' => 'datetime',
     ];
 
-    public function userPaymentMethods(): HasMany
+    public function paymentMethods(): HasMany
     {
         return $this->hasMany(PaymentMethod::class);
+    }
+
+    public function defaultPaymentMethod()
+    {
+        return $this->paymentMethods()->where('default', true)->first();
     }
 
     public function fullName(): string
